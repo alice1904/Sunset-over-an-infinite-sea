9fddf6a2a50cd1f57949349df262b4aaa0058780
Alice Jeannin <alice.jeannin@telecom-paris.fr>
2026-01-05 09:38:06 +0100

    add projects folders

---
+++ src/fragmentShaderTriangle.glsl
@@ -0,0 +1,21 @@
+#version 330 core	     // Minimal GL version support expected from the GPU
+
+struct Material {
+
+	sampler2D albedoTex;
+};
+uniform Material material;
+
+in vec3 fPosition;
+in vec3 fColor;
+
+out vec4 color;	  // Shader output: the color response attached to this fragment
+
+
+
+void main() {
+	vec3 texColor = texture(material.albedoTex, fPosition.xy).rgb;
+	color = vec4(texColor, 1.);
+	color = vec4(fColor, 1.0);
+
+}
---
+++ src/vertexShaderTriangle.glsl
@@ -0,0 +1,18 @@
+#version 330 core            // Minimal GL version support expected from the GPU
+
+layout(location=0) in vec3 vPosition;
+layout(location=1) in vec3 vColor;
+
+uniform mat4 viewMat, projMat;
+
+
+out vec3 fPosition;
+out vec3 fColor;
+
+void main() {
+        vec4 worldPosition = vec4(vPosition, 1.0);
+        fPosition = vec3(worldPosition.x, worldPosition.y, worldPosition.z)/worldPosition.a;
+        fColor = vColor;
+        gl_Position = projMat * viewMat * worldPosition; // mandatory to rasterize properly
+}
+
