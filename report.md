12cd2d6904aa4fcc454b2d4831c0370783bdaccb
Alice Jeannin <alice.jeannin@telecom-paris.fr>
2026-02-10 21:50:08 +0100

    add flat sea

--- report.md
+++ report.md
@@ -26,9 +26,7 @@ Then each time I find the closest triangle intersected by a ray, I have to brows
 
 ## Recursion and trees
 
-Unfortunately, you cannot have recursive functions in glsl. I decided to implement a tree representing the cast rays. To do so I used an array of size 
-$2**(height + 1) - 1$. The nodes are stored in the array's cells. The sons'index of the node of index i are $2*i+1$ and $2*i+2$. For each node I stored a struct containing information about the ray. I first compute the direction and origin of each ray from root to leaves and then compute the final color from leaves to root. 
+Unfortunately, you cannot have recursive functions in glsl. But ray tracing needs recursion to track all the rays obtained by refraction and reflection. I decided to implement a tree representing the cast rays. To do so I used an array of size $2**(height + 1) - 1$. The nodes are stored in the array's cells. The sons'index of the node of index i are $2*i+1$ and $2*i+2$. For each node I stored a struct containing information about the ray. I first compute the direction and origin of each ray from root to leaves and then compute the final color from leaves to root. 
 
 # TODO 
 - probleme avec redefition sortie
-- chiant car pas de fonction recursive
--- src/scene.cpp
+++ src/scene.cpp
@@ -6,46 +6,43 @@
 void Scene::init(){
 
     printf("init scene\n");
+
+    float width = 4.f;
+    float height = 0.1f;
+    float depth = 4.f;
     vertexPositions = { // the array of vertex Colors [x0, y0, z0, x1, y1, z1, ...]
-        glm::vec3(1.f, 0.f, 0.f),
-        glm::vec3(0.f, 1.f, 0.f),
-        glm::vec3(0.f, 0.f, 0.f),
 
-        glm::vec3(0.5f, 0.5f, 0.f),
-        glm::vec3(-1.f, 1.f, 0.f),
-        glm::vec3(-1.f, 0.f, 1.f),
-
-        glm::vec3(1.f, 1.f, 0.f)
+        glm::vec3(width, -height, -depth),
+        glm::vec3(-width, -height, -depth),
+        glm::vec3(width, -height, depth),
+        glm::vec3(-width, -height, depth)
 
     };
 
-    glm::vec3 edge1 = vertexPositions[4] - vertexPositions[3];
-    glm::vec3 edge2 = vertexPositions[5] - vertexPositions[3];
+    // glm::vec3 edge1 = vertexPositions[4] - vertexPositions[3];
+    // glm::vec3 edge2 = vertexPositions[5] - vertexPositions[3];
 
-    glm::vec3 normal = glm::cross(edge1, edge2);
+    // glm::vec3 normal = glm::cross(edge1, edge2);
 
-    vertexNormals = {
-        glm::vec3(0.f, 0.f, 1.f),
-        glm::vec3(0.f, 0.f, 1.f),
-        glm::vec3(0.f, 0.f, 1.f),
-        normal,
-        normal,
-        normal,
-        glm::vec3(0.f, 0.f, 1.f),
+    // vertexNormals = {
+    //     glm::vec3(0.f, 0.f, 1.f),
+    //     glm::vec3(0.f, 0.f, 1.f),
+    //     glm::vec3(0.f, 0.f, 1.f),
+    //     normal,
+    //     normal,
+    //     normal,
+    //     glm::vec3(0.f, 0.f, 1.f),
 
-    };
+    // };
 
     triangleIndices = {
-        glm::uvec3(3, 4, 5),
         glm::uvec3(0, 1, 2),
-        glm::uvec3(0, 1, 6)
+        glm::uvec3(1, 2, 3)
 
     };
 
     objectProperties = {
-        {glm::vec3(0.0, 1.0, 0.0),
-        1, 0.33, 0.33, 0.5, 0.0},
-        {glm::vec3(1.0, 0.0, 1.0),
-        3, 0.33, 0.33, 0.0, 0.8}
+        {glm::vec3(0.2, 0.2, 1.0),
+        3, 0.33, 0.33, 0.6, 0.0}
     };
 }
